a4f3694ca7737f82a6d6ab4e455651472c573325
AbhiNav8084 <shubhamkumarsingh415@gmail.comm>
2024-10-11 02:17:09 +0530

    h

--- script.js
+++ script.js
@@ -49,10 +49,10 @@ var tl = gsap.timeline({
     }
 })
 tl.to(".page1 h1", {
-    x: -1040,
+    x: -100,
 }, "anim")
 tl.to(".page1 h2", {
-    x: 1280,
+    x: 100,
     scrub:2
 }, "anim")
 tl.to(".page1 video", {
@@ -87,4 +87,40 @@ var tl3 = gsap.timeline({
 
 tl3.to(".main",{
     backgroundColor:"#0F0D0D"
+})
+
+
+var boxes = document.querySelectorAll("#box")
+boxes.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        var att = elem.getAttribute("data-image")
+        crsr.style.width = "470px"
+        crsr.style.height = "350px"
+        crsr.style.borderRadius="0"
+        crsr.style.backgroundImage = `url(${att})`
+
+    })
+    elem.addEventListener("mouseleave",function(){
+        elem.style.backgroundColor = "transparent"
+        crsr.style.width = "20px"
+        crsr.style.height = "20px"
+        crsr.style.borderRadius = "50%"
+        crsr.style.backgroundImage = `none`
+
+    })
+})
+
+
+var h4 = document.querySelectorAll("#nav h4")
+var purple = document.querySelector("#purple")
+h4.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        purple.style.display = "block"
+        purple.style.opacity = "1"
+    })
+    elem.addEventListener("mouseleave",function(){
+        purple.style.display = "none"
+        purple.style.opacity = "0"
+    })
+    
 })
